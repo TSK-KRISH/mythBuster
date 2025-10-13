825abdf177bdc4856672b347796a3b636a6851da
Rathesh <rathesh466@gmail.com>
2025-10-14 00:12:30 +0530

    backend code

---
+++ backend/src/main/java/com/example/myth/mythBuster/MythBusterApplication.java
@@ -0,0 +1,13 @@
+package com.example.myth.mythBuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythBusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythBusterApplication.class, args);
+	}
+
+}
